9d7f8c9a7c82438ab513b9c3c221c67b5e67d00d
Zinebaiterrami1 <you@example.com>
2023-10-10 12:50:06 +0200

    new commit ToDo

--- script.js
+++ script.js
@@ -1,14 +1,60 @@
 let tb = document.querySelector("#tbody");
 
-let idUser= document.querySelector("#userId").value;
-let todo= document.querySelector("#todo").value;
+let idUser= document.querySelector("#userId");
+let todo= document.querySelector("#todo");
+let addbtn = document.querySelector("#add");
 
+let userSearch = document.querySelector("#userFilter");
+let todoSearch = document.querySelector("#todoFilter");
+
+
+let data = [];
+userSearch.addEventListener("input",()=>{
+    console.log("User :",userSearch.value);
+    //let fdata = data.filter(e=>e.userId.toString().includes(userSearch.value));
+    let fdata = data.filter(e=>e.userId==parseInt(userSearch.value));
+    
+    showlist(fdata,tb);
+});
+
+todoSearch.addEventListener("input",()=>{
+    console.log("Todo :",todoSearch.value);
+    let fdata = data.filter(e=>e.title.includes(todoSearch.value));
+    showlist(fdata,tb);
+
+});
+
+
+addbtn.addEventListener("click",() =>{
+    let newTodo = {};
+    newTodo.id=data[0].id+1;
+    newTodo.userId=idUser.value;;
+    newTodo.title=todo.value;
+    newTodo.completed=false;
+
+   /* let nbrows =tb.rows.length;
+    let row = table.insertRow(nbrows+1);
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+    let cell3=  row.insertCell(2);
+    let cell4 = row.insertCell(3);
+
+    cell1.innerHTML = `${nbrows+1}`;
+    cell2.innerHTML = `${idUser}`;
+    cell3.innerHTML = `${todo}`;
+    cell4.innerHTML = `<input type="checkbox"}/>`;*/
+    data.unshift(newTodo);
+    showlist(data,tb);
+
+
+});
 
 let req = new XMLHttpRequest();
 req.open("GET","https://jsonplaceholder.typicode.com/todos");
 
 function showlist(data,container){
     let txt="";
+     
     data.forEach(tr => {
     txt =txt +`<tr>
                  <td>${tr.id}</td>
@@ -16,30 +62,18 @@ function showlist(data,container){
                  <td>${tr.title}</td>
                  <td><input type="checkbox" ${tr.completed ? 'checked' : ""}/></td>
                  </tr>`;
-     container.innerHTML=txt;
+    
   }); 
+  container.innerHTML=txt;
 }
 
 req.onreadystatechange=()=>{
     console.log(req.readyState);
     if(req.readyState==4 && req.status == 200){
        let resp=JSON.parse(req.responseText); 
-       let l = resp.sort((a,b)=>b.id-a.id)
-        showlist(l,tb);
+        data = resp.sort((a,b)=>b.id-a.id)
+        showlist(data,tb);
 
-        let addbtn = document.querySelector("#add");
-        addbtn.addEventListener("click",() =>{
-            let nbrows =tb.rows.length;
-            console.log(nbrows);
-        /* let txt = tb.innerHTML ;
-            txt = txt + `<tr>
-                        <td>201</td>
-                        <td>${idUser}</td>
-                        <td>${todo}</td>
-                        <td><input type="checkbox"}/></td>
-                        </tr>`;
-            tb.innerHTML = txt ;*/
-        });
     }
 }
 req.send();
